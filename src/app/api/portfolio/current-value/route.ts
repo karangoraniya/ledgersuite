544b03e8a29ca96d279e027a20931f5f680a9403
Goraniya Karan <karangoraniya@gmail.com>
2024-11-17 04:51:31 +0700

    feat : 1 inch api added 🚀

---
+++ src/app/api/portfolio/current-value/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+const BASE_URL = "https://api.1inch.dev/portfolio/portfolio/v4";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const address = searchParams.get("address");
+  const chainId = searchParams.get("chainId");
+
+  if (!address) {
+    return NextResponse.json({ error: "Address is required" }, { status: 400 });
+  }
+
+  try {
+    const response = await fetch(
+      `${BASE_URL}/overview/erc20/current_value?addresses=${address}&chain_id=${chainId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${API_KEY}`,
+        },
+      }
+    );
+
+    const data = await response.json();
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error("Error fetching current value data:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch portfolio data" },
+      { status: 500 }
+    );
+  }
+}
