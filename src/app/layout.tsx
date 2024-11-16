a0a918731a0529764652d7681bb8f79f7e15a646
Goraniya Karan <karangoraniya@gmail.com>
2024-11-16 20:53:39 +0700

    feat : shadcn components added and config 🚀

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { NetworkProvider } from "@/context/NetworkContext";
+import { Toaster } from "sonner";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <NetworkProvider>
+          {children}
+          <Toaster richColors closeButton position="top-right" />
+        </NetworkProvider>
       </body>
     </html>
   );
